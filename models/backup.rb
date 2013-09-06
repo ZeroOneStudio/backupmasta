e3d6fd3b8900151c79dd6631a00a071510c907b9
Sergey Kishenin <sergey.kishenin@gmail.com>
2013-09-06 14:12:08 +0600

    let's use public key instead of ssh pass

--- models/backup.rb
+++ models/backup.rb
@@ -5,15 +5,14 @@ class Backup
 
   property :id, Serial
   property :ssh_user, String
-  property :ssh_password, String
-  property :db_host, String
+  property :host, String
   property :db_user, String
   property :db_name, String
   property :db_password, String
 
   def perform_backup
     puts "Logging in ..."
-    Net::SSH.start(db_host, ssh_user, password: ssh_password) do |ssh|
+    Net::SSH.start(host, ssh_user) do |ssh|
       dump_name = "#{db_name}_dump_#{Time.now.strftime('%d_%m_%Y_%H_%M')}"
       mysqldump_command = "mysqldump --user=#{db_user} --password=#{db_password} #{db_name} > #{dump_name}.sql"
       
