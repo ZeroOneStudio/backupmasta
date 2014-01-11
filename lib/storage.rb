bc3c183bdb6c9271a36a60cca5b22bc08a832646
Sergey Kishenin <sergey.kishenin@gmail.com>
2014-01-11 13:19:54 +0600

    DRY storage.rb

--- lib/storage.rb
+++ lib/storage.rb
@@ -13,24 +13,26 @@ class Storage
     attr_accessor :name
 
     def create
-      connection.directories.create({key: "backupmasta-#{name}-#{Sinatra::Base.environment}"})
+      directories.create({key: "backupmasta-#{name}-#{Sinatra::Base.environment}"})
     end
 
     def destroy
-      begin
-        get_directory.files.map(&:destroy)
-        get_directory.destroy
+      get_directory.files.map(&:destroy)
+      get_directory.destroy
       rescue
-        return
-      end
+      return
+    end
+    
+    def files
+      get_directory.files
     end
 
     def get_directory
-      connection.directories.get("backupmasta-#{name}-#{Sinatra::Base.environment}")
+      directories.get("backupmasta-#{name}-#{Sinatra::Base.environment}")
     end
 
-    def files
-      get_directory.files
+    def directories
+      connection.directories
     end
   end
 end
